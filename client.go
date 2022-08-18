eba8798ef333e9404ca135d262fd261870f56da9
a very fake floordiv <real.floordiv@gmail.com>
2022-08-18 12:53:33 +0200

    minor codestyle improvements
    removed useless newlines where you don't need them, removed pass-by-reference where you also don't need, renamed all the receivers to cl (for consistency)

--- client.go
+++ client.go
@@ -14,9 +14,9 @@ import (
 
 type ClientOptions map[string]interface{}
 
-func (opt *ClientOptions) Merge(other ClientOptions) {
+func (c ClientOptions) Merge(other ClientOptions) {
 	for key := range other {
-		(*opt)[key] = other[key]
+		c[key] = other[key]
 	}
 }
 
@@ -30,30 +30,30 @@ func New() *Request {
 	return NewRequest(Cleaned())
 }
 
-func Default() (cl *StandardClient) {
+func Default() *StandardClient {
 	return &StandardClient{
 		Client: &http.Client{},
 		Logger: defaultLogger,
 	}
 }
 
-func Cleaned() (cl *StandardClient) {
+func Cleaned() *StandardClient {
 	return &StandardClient{
 		Client: cleanhttp.DefaultClient(),
 		Logger: defaultLogger,
 	}
 }
 
-func Pooled() (cl *StandardClient) {
+func Pooled() *StandardClient {
 	return &StandardClient{
 		Client: cleanhttp.DefaultPooledClient(),
 		Logger: defaultLogger,
 	}
 }
 
-func NewClient(clients ...*http.Client) (cl *StandardClient) {
-
+func NewClient(clients ...*http.Client) *StandardClient {
 	var client *http.Client
+
 	if len(clients) == 0 {
 		client = &http.Client{}
 	} else {
@@ -66,66 +66,57 @@ func NewClient(clients ...*http.Client) (cl *StandardClient) {
 	}
 }
 
-func (client *StandardClient) Error() error {
-	return client.err
+func (cl StandardClient) Error() error {
+	return cl.err
 }
 
-func (client *StandardClient) With(options ...interface{}) *StandardClient {
-
+func (cl *StandardClient) With(options ...interface{}) *StandardClient {
 	for _, option := range options {
 		switch option.(type) {
 		case time.Duration:
-			client.Timeout = option.(time.Duration)
+			cl.Timeout = option.(time.Duration)
 
 		case http.RoundTripper:
-			client.Transport = option.(http.RoundTripper)
+			cl.Transport = option.(http.RoundTripper)
 
 		case http.CookieJar:
-			client.Jar = option.(http.CookieJar)
+			cl.Jar = option.(http.CookieJar)
 		}
 	}
-	return client
+	return cl
 }
 
-func (client *StandardClient) WithTimeout(
-	timeout time.Duration) *StandardClient {
-
-	client.Timeout = timeout
-	return client
+func (cl *StandardClient) WithTimeout(timeout time.Duration) *StandardClient {
+	cl.Timeout = timeout
+	return cl
 }
 
-func (client *StandardClient) WithJar(jar http.CookieJar) *StandardClient {
-
-	client.Jar = jar
-	return client
+func (cl *StandardClient) WithJar(jar http.CookieJar) *StandardClient {
+	cl.Jar = jar
+	return cl
 }
 
-func (client *StandardClient) SetCookies(
-	host string, cookies ...*http.Cookie) *StandardClient {
-
-	if client.Jar == nil {
-		client.Jar, _ = cookiejar.New(nil)
+func (cl *StandardClient) SetCookies(host string, cookies ...*http.Cookie) *StandardClient {
+	if cl.Jar == nil {
+		cl.Jar, _ = cookiejar.New(nil)
 	}
 
 	u, _ := url.Parse(host)
-	client.Jar.SetCookies(u, cookies)
-	return client
+	cl.Jar.SetCookies(u, cookies)
+	return cl
 }
 
-func (client *StandardClient) Cookies(host string) []*http.Cookie {
+func (cl StandardClient) Cookies(host string) []*http.Cookie {
 	u, _ := url.Parse(host)
-	return client.Jar.Cookies(u)
+	return cl.Jar.Cookies(u)
 }
 
-func (client *StandardClient) WithTransport(
-	transport http.RoundTripper) *StandardClient {
-
-	client.Transport = transport
-	return client
+func (cl *StandardClient) WithTransport(transport http.RoundTripper) *StandardClient {
+	cl.Transport = transport
+	return cl
 }
 
-func (client *StandardClient) WithLogger(logger Logger) *StandardClient {
-
-	client.Logger = logger
-	return client
+func (cl *StandardClient) WithLogger(logger Logger) *StandardClient {
+	cl.Logger = logger
+	return cl
 }
